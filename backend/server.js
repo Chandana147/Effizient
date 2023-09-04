e1816694aab7d041358307bc4566d800b99b843e
Chaitanya Chandana Kakara <109449619+Chandana147@users.noreply.github.com>
2023-09-04 20:21:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ console.log(process.env.User)
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
   })
 );
 mongoose.connect(process.env.MONGO_STRING, {
